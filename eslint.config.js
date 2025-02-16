b557f2339f0e61534560e13f7d957c2e884962f4
KanoZe <zectec75@gmail.com>
2025-02-16 12:15:30 +0200

    Fix formatting issues with Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -7,12 +7,19 @@ export default [
       ecmaVersion: 'latest',
       sourceType: 'module',
       globals: {
-        structuredClone: 'readonly', // Declare structuredClone as a global here
+        structuredClone: 'readonly', // Declare structuredClone as a global
       },
     },
     rules: {
-      'no-unused-vars': 'warn',
+      'no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          args: 'after-used',
+          ignoreRestSiblings: true,
+        },
+      ],
       'no-console': 'off',
     },
   },
-]
+];
