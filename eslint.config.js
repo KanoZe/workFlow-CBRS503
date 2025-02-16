d2269bf6406c20aff0cb3b86988f1061bab6dc31
KanoZe <zectec75@gmail.com>
2025-02-16 12:07:27 +0200

    message

--- eslint.config.js
+++ eslint.config.js
@@ -7,9 +7,12 @@ export default [
       ecmaVersion: 'latest',
       sourceType: 'module',
     },
+    globals: {
+      structuredClone: 'readonly', // Declare structuredClone as a global
+    },
     rules: {
       'no-unused-vars': 'warn',
       'no-console': 'off',
     },
   },
-]
+];
